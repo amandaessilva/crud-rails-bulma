e4e8a82439191befbbe897c4aff2aee7072fdd70
amandaessilva <amanda.elensantos@gmail.com>
2024-01-20 12:13:36 -0300

    ajustes responsividades

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('deleteButton').addEventListener('click', function(event) {
+    if (!confirm('Tem certeza que deseja deletar este produto?')) {
+      event.preventDefault();
+    }
+  });
+});
